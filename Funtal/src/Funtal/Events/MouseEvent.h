435b0fdbd262afc23252fdc7482a1307b0f8778c
Bonington <yikun.chen@163.com>
2022-10-24 21:57:54 +0800

    构建事件系统-Alpha1

--- Funtal/src/Funtal/Events/MouseEvent.h
+++ Funtal/src/Funtal/Events/MouseEvent.h
@@ -38,4 +38,78 @@ namespace Funtal
         float m_MouseX;
         float m_MouseY;
     };
+
+    class FUNTAL_API MouseScrolledEvent : public Event
+    {
+    public:
+        MouseScrolledEvent(float xOffset, float yOffset)
+            : m_XOffset(xOffset), m_YOffset(yOffset)
+        {}
+
+        [[nodiscard]] inline float GetXOffset() const { return m_XOffset; }
+        [[nodiscard]] inline float GetYOffset() const { return m_YOffset; }
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseScrolledEvent: " << GetXOffset() << ", " << GetYOffset();
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseScrolled)
+        EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+
+    private:
+        float m_XOffset;
+        float m_YOffset;
+    };
+
+    class FUNTAL_API MouseButtonEvent : public Event
+    {
+    public:
+        [[nodiscard]] inline int GetMouseButton() const { return m_Button; }
+
+        EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+
+    protected:
+        explicit MouseButtonEvent(int button)
+            : m_Button(button)
+        {}
+
+        int m_Button;
+    };
+
+    class FUNTAL_API MouseButtonPressedEvent : public MouseButtonEvent
+    {
+    public:
+        explicit MouseButtonPressedEvent(int button)
+            : MouseButtonEvent(button)
+        {}
+
+        [[nodiscard]] std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseButtonPressedEvent: " << m_Button;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseButtonPressed)
+    };
+
+    class FUNTAL_API MouseButtonReleasedEvent : public MouseButtonEvent
+    {
+    public:
+        explicit MouseButtonReleasedEvent(int button)
+            : MouseButtonEvent(button)
+        {}
+
+        [[nodiscard]] std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "MouseButtonReleasedEvent: " << m_Button;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(MouseButtonReleased)
+    };
 }
