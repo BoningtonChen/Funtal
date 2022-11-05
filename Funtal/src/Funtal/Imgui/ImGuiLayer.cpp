f128b7706354ecc1baf2b1f7609b5f0f8c079254
Bonington <yikun.chen@163.com>
2022-11-05 10:44:31 +0800

    引入ImGui，生成跨平台OpenGL的ImGui依赖

---
+++ Funtal/src/Funtal/Imgui/ImGuiLayer.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 陈奕锟 on 2022/11/5.
+//
+#include "ftpch.h"
+#include "ImGuiLayer.h"
+
+namespace Funtal
+{
+
+    ImGuiLayer::ImGuiLayer()
+        : Layer("ImGuiLayer")
+    {}
+
+    ImGuiLayer::~ImGuiLayer()
+    {}
+
+    void ImGuiLayer::OnAttach()
+    {
+
+    }
+
+    void ImGuiLayer::OnDetach()
+    {}
+
+    void ImGuiLayer::OnUpdate()
+    {
+
+    }
+
+    void ImGuiLayer::OnEvent(Event &event)
+    {
+
+    }
+
+}
---
+++ Funtal/src/Funtal/Imgui/ImGuiLayer.h
@@ -0,0 +1,29 @@
+//
+// Created by 陈奕锟 on 2022/11/5.
+//
+
+#ifndef FUNTAL_IMGUILAYER_H
+#define FUNTAL_IMGUILAYER_H
+
+#endif //FUNTAL_IMGUILAYER_H
+#pragma once
+
+#include <Funtal/Layer.h>
+
+namespace Funtal
+{
+    class ImGuiLayer : public Layer
+    {
+    public:
+        ImGuiLayer();
+        ~ImGuiLayer() override;
+
+        void OnAttach() override;
+        void OnDetach() override;
+        void OnUpdate() override;
+        void OnEvent(Event& event) override;
+
+    private:
+
+    };
+}
