e644c4d8142e822d95247a939b985cf3068bdf65
Bonington <yikun.chen@163.com>
2022-12-22 11:27:29 +0800

    完成ImGui窗口监视：更改背景

--- Funtal/src/Funtal/Application.cpp
+++ Funtal/src/Funtal/Application.cpp
@@ -61,7 +61,7 @@ namespace Funtal
     {
         while (m_Running)
         {
-            glClearColor(1, 0, 1, 1);
+            glClearColor(1, 1, 1, 1);
             glClear(GL_COLOR_BUFFER_BIT);
 
             for (Layer* layer : m_LayerStack)
